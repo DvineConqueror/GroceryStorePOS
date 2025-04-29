c29fc4bd880fcec5a7fe771ac9c6f75e98b8438c
Henry Alicoben <114635034+DvineConqueror@users.noreply.github.com>
2025-04-29 23:35:10 +0800

    Updates:
    Made the website to a progressive web app.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,6 +11,35 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+       name: 'Grocery Store POS',
+       short_name: 'Grocery POS',
+       description: 'A simple grocery store POS system',
+       theme_color: '#ffffff',
+       icons: [
+         {
+           src: 'pwa-192x192.png',
+           sizes: '192x192',
+           type: 'image/png'
+         },
+         {
+           src: 'pwa-512x512.png',
+           sizes: '512x512',
+           type: 'image/png' 
+         },
+         {
+           src: 'pwa-512x512.png',
+           sizes: '512x512',
+           type: 'image/png',
+           purpose: 'any maskable'
+         },
+
+       ]
+      }
+    })
   ],
   resolve: {
     alias: {
